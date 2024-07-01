3a6f0d6374de04caa0f5e8c5c8ee6092e6f2c00a
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-07-01 15:41:07 -0300

    Script Assícrono

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,11 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <link rel="icon" href="/icon_title.png" ></link>
-      <script src="https://kit.fontawesome.com/29712bfbae.js" crossOrigin="anonymous"></script>
+      <link rel="icon" href="/icon_title.png" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet" />
+      {/* Script FontAwesome carregado de forma assíncrona */}
+      <script async src="https://kit.fontawesome.com/29712bfbae.js" crossOrigin="anonymous"></script>
       <body>
         <Main />
         <NextScript />
