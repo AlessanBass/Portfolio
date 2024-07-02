4c232a6e0f7f5d865592a83b92113ae314e72a57
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-07-02 19:23:59 -0300

    Galeria Implementada

--- src/pages/projeto/[id]/index.tsx
+++ src/pages/projeto/[id]/index.tsx
@@ -1,11 +1,9 @@
 import Header from "@/components/Header";
 import { useRouter } from "next/router";
 import projetosData from "../../../../public/projetos/projetos.json";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import style from "@/styles/projetoIndividual.module.css";
-import Image from "next/image";
 import React from "react";
-import { Galleria } from 'primereact/galleria';
 
 interface Projeto {
     projetoId: number;
@@ -21,8 +19,7 @@ export default function Projeto() {
     const { id } = router.query;
     const [projeto, setProjeto] = useState<Projeto | null>(null);
     const [images, setImages] = useState<any[]>([]);
-    const [activeIndex, setActiveIndex] = useState(0);
-    const galleria = useRef<any>(null);
+    const [currentImage, setCurrentImage] = useState<number | null>(null);
 
     useEffect(() => {
         if (id) {
@@ -41,14 +38,21 @@ export default function Projeto() {
         }
     }, [id]);
 
-    const itemTemplate = (item: { itemImageSrc: string | undefined; alt: string | undefined; }) => {
-        return <img src={item.itemImageSrc} alt={item.alt} style={{ width: '100%', display: 'block' }} />;
-    }
+    const openModal = (imageIndex: number) => {
+        setCurrentImage(imageIndex);
+    };
 
-    const thumbnailTemplate = (item: { thumbnailImageSrc: string | undefined; alt: string | undefined; }) => {
-        return <img src={item.thumbnailImageSrc} alt={item.alt} style={{ display: 'block' }} />;
-    }
+    const closeModal = () => {
+        setCurrentImage(null);
+    };
 
+    const showNextImage = () => {
+        setCurrentImage((prevImage) => (prevImage! + 1) % images.length);
+    };
+
+    const showPrevImage = () => {
+        setCurrentImage((prevImage) => (prevImage! - 1 + images.length) % images.length);
+    };
 
     if (!projeto) {
         return <div>Carregando...</div>;
@@ -57,8 +61,8 @@ export default function Projeto() {
     return (
         <>
             <Header />
-            <div className={`${style.section1}}`}>
-                <h1 className={`${style.h1}`}> INFORMAÇÕES</h1>
+            <div className={`${style.section1}`}>
+                <h1 className={`${style.h1}`}>INFORMAÇÕES</h1>
                 <div className={style.containerPrincipal}>
                     <img className={`${style.img}`} src={projeto.imageTitle} alt={projeto.title} />
                     <div className={`${style.containerParagrafo}`}>
@@ -67,53 +71,49 @@ export default function Projeto() {
                                 {index === 0 ? <h2 className={style.pTitle}>{projeto.title}</h2> : null}
                                 <p className={style.p}>{texto}</p>
                             </React.Fragment>
-
                         ))}
                     </div>
                 </div>
             </div>
-            {/* SECTION 2: Tecnologias Utilizadas - Links*/}
             <div className={`${style.section2}`}>
-                <h1 className={`${style.h1}`}> TECNOLOGIAS UTILIZADAS</h1>
+                <h1 className={`${style.h1}`}>TECNOLOGIAS UTILIZADAS</h1>
             </div>
-
-            {/* SECTION 3: Galeria */}
             <div className={`${style.section1}`}>
-                <h1 className={`${style.h1}`}> GALERIA DE IMAGENS</h1>
-                {projeto && (
-                    <div>
-                        {images && images.length > 0 && (
-                            <div className={`${style.containerPai}`}>
-                                <Galleria className={`${style.gallery} ${style.customgalleria}`} ref={galleria} value={images} numVisible={7} style={{ maxWidth: '100%' }}
-                                    activeIndex={activeIndex} onItemChange={(e) => setActiveIndex(e.index)}
-                                    circular fullScreen showItemNavigators showThumbnails={false}
-                                    item={itemTemplate} thumbnail={thumbnailTemplate} 
-                                    
-                                    />
-                                <div className={`${style.containerImgs}`}>
-                                    {images.map((image, index) => (
-                                        <div className="" key={index}>
-                                            <img className={`${style.imgGalerry}`} src={image.thumbnailImageSrc} alt={image.alt} style={{ cursor: 'pointer' }} onClick={() => {
-                                                setActiveIndex(index);
-                                                if (galleria.current) {
-                                                    setActiveIndex(index);
-                                                    galleria.current.show();
-                                                }
-                                            }} />
-                                        </div>
-                                    ))}
-                                </div>
+                <h1 className={`${style.h1}`}>GALERIA DE IMAGENS</h1>
+                <div>
+                    {images && images.length > 0 && (
+                        <div className={`${style.containerPai}`}>
+                            <div className={`${style.containerImgs}`}>
+                                {images.map((image, index) => (
+                                    <div key={index}>
+                                        <img
+                                            className={`${style.imgGalerry}`}
+                                            src={image.thumbnailImageSrc}
+                                            alt={image.alt}
+                                            style={{ cursor: 'pointer' }}
+                                            onClick={() => openModal(index)}
+                                        />
+                                    </div>
+                                ))}
                             </div>
-                        )}
-                    </div>
-                )}
+                        </div>
+                    )}
+                </div>
             </div>
+            {currentImage !== null && (
+                <div className={style.modal} onClick={closeModal}>
+                    <span className={style.closeButton} onClick={closeModal}>&times;</span>
+                    <img className={style.modalContent} src={images[currentImage].thumbnailImageSrc} alt={images[currentImage].alt} />
+                    <div className={style.controls}>
+                        <button onClick={(e) => { e.stopPropagation(); showPrevImage(); }}>
+                            <i className="fa-solid fa-angle-left"></i>
+                        </button>
+                        <button onClick={(e) => { e.stopPropagation(); showNextImage(); }}>
+                            <i className="fa-solid fa-angle-right"></i>
+                        </button>
+                    </div>
+                </div>
+            )}
         </>
     );
 }
-
-{/* <div className={style.imageGallery}>
-                    {projeto.imagens?.map((imagem, index) => (
-                        <Image key={index} src={imagem} alt={`Imagem ${index + 1}`} width={500} height={300} />
-                    ))}
-                </div> */}
